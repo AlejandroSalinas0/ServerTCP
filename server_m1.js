4bcd11746217344fb6474ea87f262e74cbf5bf29
AlejandroSalinas0 <77340125+AlejandroSalinas0@users.noreply.github.com>
2021-01-14 09:29:34 -0600

    Add files via upload

---
+++ server_m1.js
@@ -0,0 +1,43 @@
+const net = require('net');
+const port = 1339;
+const host = '127.0.0.1';
+
+const server = net.createServer();
+server.listen(port, host, () => {
+    console.log('TCP Server is running on port ' + port + '.');
+});
+
+let sockets = [];
+
+server.on('connection', function(sock) {
+    console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
+    sockets.push(sock);
+
+    sock.on('data', function(data) {
+        console.log('DATA ' + sock.remoteAddress + ': ' + data);
+        const str=data;
+        const word=str.split(' ');
+        console.log('NEW '+ word)
+        // Write the data back to all the connected, the client will receive it as data from the server
+        sockets.forEach(function(sock, index, array) {
+            //sock.write('hola');
+            sock.write(sock.remoteAddress + ':' + sock.remotePort + " N1\r\n " + data + '\r\n');
+            //sock.write('N1\r\n');
+           // const dato= 'The quick brown fox jumps over the lazy dog.';
+             // const str=data;
+            //console.log('CADENA CORTADA EXITOSAMENTE: ' + str[0]);
+        });
+    }); 
+    sock.on('error', function(err) {
+        console.log(`Error: ${err}`);
+    });
+
+    // Add a 'close' event handler to this instance of socket
+    sock.on('close', function(data) {
+        let index = sockets.findIndex(function(o) {
+            return o.remoteAddress === sock.remoteAddress && o.remotePort === sock.remotePort;
+        })
+        if (index !== -1) sockets.splice(index, 1);
+        console.log('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort + '\r\n');
+    });
+});
